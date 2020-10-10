8855d6baf312e4a78d829591989134d6bbda45e9
fkakatie <sextonskatie@gmail.com>
2020-10-10 09:42:57 -0600

    prepare for heroku deploy

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const path = require("path");
 
 // Initialize the app and create a port
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+})
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
